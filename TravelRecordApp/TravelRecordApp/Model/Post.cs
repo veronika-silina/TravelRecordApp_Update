6b1576ab0b3ea5a96c40540ce27f8f605464abff
Eduardo Rosas <eduardorosas@MacBook-Pro-de-Eduardo.local>
2019-02-11 20:12:13 -0600

    Before update functionality

---
+++ TravelRecordApp/TravelRecordApp/Model/Post.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelRecordApp.Model
+{
+    public class Post
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(250)]
+        public string Experience { get; set; }
+    }
+}
