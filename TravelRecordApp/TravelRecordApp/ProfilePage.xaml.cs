be7b9a553c66c4a8f473d42cdf6beb01237bc92a
Eduardo Rosas <lalorosas@outlook.com>
2019-10-09 07:26:12 -0500

    1641

---
+++ TravelRecordApp/TravelRecordApp/ProfilePage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelRecordApp.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ProfilePage : ContentPage
+	{
+		public ProfilePage ()
+		{
+			InitializeComponent ();
+		}
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                var postTable = conn.Table<Post>().ToList();
+
+                var categories = (from p in postTable
+                                  orderby p.CategoryId
+                                  select p.CategoryName).Distinct().ToList();
+
+                Dictionary<string, int> categoriesCount = new Dictionary<string, int>();
+                foreach(var category in categories)
+                {
+                    var count = (from post in postTable
+                                 where post.CategoryName == category
+                                 select post).ToList().Count;
+
+                    categoriesCount.Add(category, count);
+                }
+
+                postCountLabel.Text = postTable.Count.ToString();
+            }
+        }
+    }
+}
